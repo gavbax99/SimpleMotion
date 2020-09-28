df548e37438d5a2fac24c03f5967f816df11948a
gavbax99 <gav.bax@gmail.com>
2020-09-27 18:51:49 -0700

    improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,24 @@ const scrollIntoViewDenom = 0.85;
 // setup anim array
 let animElementArr = document.querySelectorAll("[data-sm]");
 
-
 // setup anim effects for the nodelist
 animElementArr.forEach(ele => {
     const transitionTime = ele.getAttribute("data-sm-time");
     const eastType = ele.getAttribute("data-sm-ease");
     const startingOpacity = ele.getAttribute("data-sm-opac");
     const distanceToTravel = ele.getAttribute("data-sm-dist");
+    // delay before anim starts
 
-    ele.style.transitionTimingFunction = eastType ? `${eastType}` : "ease-in";
+    // BUG:
+    // there is a bug where when refreshing the page and there are potentially items that could
+    // be rendered at the bottom (near the threshold), since the items pre-render out of place some
+    // of their tops are below the trigger zone.
+    // FIX: set css shit first then transition so it doesnt ahve to wait
+    
+
+    ele.style.transitionTimingFunction = eastType ? `${eastType}` : "ease-out";
     ele.style.opacity= startingOpacity ? `${startingOpacity}` : "0";
-    ele.style.transform = distanceToTravel ? `translate(0, ${distanceToTravel})` : "translate(0, 40px)";
+    ele.style.transform = distanceToTravel ? `translate(0, ${distanceToTravel}px)` : "translate(0, 40px)";
     ele.style.transition = transitionTime ? `transform ${transitionTime}` : "transform 750ms";
 });
 
@@ -46,21 +53,30 @@ scrollFadeHandler = () => {
     const windowInnerHeight = window.innerHeight;
 
     // counter to determine what to remove from the array
-    let counter = 0;
+    // let counter = 0;
     
     // for each element in the nodelist, check to see if it 
     // should be faded. if so, fade it in and increment the counter
     [...animElementArr].forEach((ele, i) => {
-        eleTop = ele.getBoundingClientRect().top + window.scrollY;
+        eleTop = ele.getBoundingClientRect().top + window.scrollY + ele.getAttribute("data-sm-dist");
         if (window.scrollY > (eleTop - windowInnerHeight * scrollIntoViewDenom)) {
             ele.style.opacity= "1";
             ele.style.transform = "translate(0, 0)";
-            counter++;
+            ele.setAttribute("data-sm", "complete");
+            
+            
+            // counter++;
         }
     });
+    
 
     // then remove it/the group from the array
-    animElementArr = [].slice.call(animElementArr, counter);
-}
+    animElementArr = [...animElementArr].filter.call(ele => {
+        return ele.getAttribute("data-sm", "complete");
+    });
+
+
+    // animElementArr = [].slice.call(animElementArr, counter);
+};
 
 scrollFadeHandler();
